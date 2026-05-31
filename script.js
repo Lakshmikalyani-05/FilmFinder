1393c173fc78bfde01000dfb0694c52bfc0a16ed
Palagani Lakshmi kalyani <palaganilakshmikalyani@gmail.com>
2026-05-31 10:06:19 +0530

    Day 4 - Added More Movies
    Added RRR, 3 Idiots, and Inception movie cards. Improved movie collection and verified language filtering and search functionality.

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function showMovies(language) {
         }
     });
 }
+function searchMovies() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let movies = document.querySelectorAll(".movie-card");
+
+    movies.forEach(function(movie) {
+        let movieName = movie.querySelector("h3").innerText.toLowerCase();
+
+        if (movieName.includes(input)) {
+            movie.style.display = "block";
+        } else {
+            movie.style.display = "none";
+        }
+    });
+}
