1818259ff86ab84917ea04214675404abbe90df1
Palagani Lakshmi kalyani <palaganilakshmikalyani@gmail.com>
2026-06-01 10:16:50 +0530

    Day 13 - Created First Movie API
    Successfully created the first REST API endpoint using Flask. Added movie data in JSON format and implemented the /movies route to serve movie information from the backend. This marks the first step toward integrating the FilmFinder frontend with backend services.

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+movies = [
+    {
+        "name": "Sita Ramam",
+        "language": "Telugu",
+        "category": "Romance",
+        "rating": 8.5
+    },
+    {
+        "name": "RRR",
+        "language": "Telugu",
+        "category": "Action",
+        "rating": 8.0
+    },
+    {
+        "name": "Dangal",
+        "language": "Hindi",
+        "category": "Sports Drama",
+        "rating": 8.3
+    }
+]
+
 @app.route("/")
 def home():
     return "FilmFinder Backend is Working!"
+
+@app.route("/movies")
+def get_movies():
+    return movies
 
 if __name__ == "__main__":
     app.run(debug=True)
